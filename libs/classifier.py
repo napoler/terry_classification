e94b4cfed4ed19ec97c45e41622355ae631b8609
Terry chan <napoler2008@gmail.com>
2019-11-03 11:21:48 +0800

    Signed-off-by: Terry chan <napoler2008@gmail.com>

--- libs/classifier.py
+++ libs/classifier.py
@@ -6,7 +6,10 @@ from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM,Be
 
 # OPTIONAL: if you want to have more information on what's happening, activate the logger as follows
 import logging
+import Terry_toolkit as tkit
 logging.basicConfig(level=logging.INFO)
+from collections import Counter
+
 
 class Classifier:
     """预测分类"""
@@ -44,28 +47,30 @@ class Classifier:
         segment_ids += padding
 
         
-        labels = self.tokenizer.convert_tokens_to_ids(['0','1'])
+        labels = self.tokenizer.convert_tokens_to_ids(['other','pet'])
         # labels
         
         
         input_ids_tensor = torch.tensor([input_ids])
         input_mask_tensor = torch.tensor([input_mask])
         segment_ids_tensor = torch.tensor([segment_ids])
-        labels_tensor = torch.tensor([labels])
+        # labels_tensor = torch.tensor([labels])
         
         device='cpu'
         
         input_ids_tensor = input_ids_tensor.to(device)
         input_mask_tensor = input_mask_tensor.to(device)
         segment_ids_tensor = segment_ids_tensor.to(device)
-        labels_tensor = labels_tensor.to(device)
+        # labels_tensor = labels_tensor.to(device)
+
+
         # print(input_ids_tensor)
         # print(input_mask_tensor)
         # print(segment_ids_tensor)
         # print(labels_tensor)
         with torch.no_grad():
             logits = self.model(input_ids_tensor,segment_ids_tensor,input_mask_tensor )
-            print(logits)
+            # print(logits)
             return logits
     def classifier_label(self,logits):
     #     k = logits.view(-1, 2)
@@ -74,12 +79,12 @@ class Classifier:
         preds = np.argmax(n, axis=1)
         return preds
 
-    def yuce(self,text):
+    def prediction(self,text):
         
         logits = self.c(text)
         y = self.classifier_label(logits)
         return y
-    def yuce_list(self,text_list):
+    def prediction_list(self,text_list):
 #         text_list=[
 #     "其实铲屎官们常常有种错觉，养了喵跟没养差不多，平时基本都很难看到它们，唯一例外的是饭点。",
 #     '北交大原校长宁滨遇车祸去世 其座驾变道与旁车接触后失控翻滚',
@@ -88,8 +93,56 @@ class Classifier:
 #     '喵星人的食物以什么为主才是最好的？没有最好的，只有适合的'
     
 # ]
-
+        data=[]
         for item in text_list:
-            print(item)
-            print(self.yuce(item))
-            print("##"*50)
+            pred= self.prediction(item)
+            # print(item)
+            # print(pred)
+            # print("##"*50)
+            data.append(pred[0])
+        return data
+    def article_prediction(self,article):
+        tx = tkit.Text()
+        text_list= tx.sentence_segmentation(article)
+        return self.prediction_list(text_list)
+
+    def proportion(self,full,element):
+        """
+        计算元素所占比例"""
+        # full.self.article_prediction(article)
+        return full.count(element)/len(full)
+
+    def proportion_article(self,article,element):
+        """
+        预测文章中某个分类所占比例"""
+        full =self.article_prediction(article)
+        self.top_three(full)
+        return self.proportion(full,element)
+    def proportion_article_auto(self,article):
+        """
+        预测文章各种类别所占比重"""
+        full =self.article_prediction(article)
+        l = self.top_three(full)
+        data = []
+        for label,num in l:
+            
+            it ={
+                'label':label,
+                'proportion':self.proportion(full,label)
+
+            }
+            data.append(it)
+        return data
+
+
+
+
+
+    def top_three(self,full):
+        """计算数组中出现次数最多的元素"""
+
+        lab_counts = Counter(full)
+        # 出现频率最高的3个单词
+        top_three = lab_counts.most_common(3)
+        # print(top_three)
+        return top_three
